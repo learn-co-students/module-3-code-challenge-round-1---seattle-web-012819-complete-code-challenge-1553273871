104bf9415c1847b38fd5d75e8e18bc3d523e298a
ScottDenton <scottyd_14@hotmail.com>
2019-03-22 11:27:28 -0700

    All specs met, CSS could use some work

--- src/index.js
+++ src/index.js
@@ -2,20 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
   let imageId = 2282 //Enter the id from the fetched image here
-
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
-
   const likeURL = `https://randopic.herokuapp.com/likes/`
-
   const commentsURL = `https://randopic.herokuapp.com/comments/`
-
   const myImage = document.getElementById('image')
   const imageName = document.getElementById('name')
   const imageLikes = document.getElementById('likes')
   const imageCommentsUl = document.getElementById('comments')
   const likeButton = document.getElementById('like_button')
   const commentForm = document.getElementById('comment_form')
+  commentForm.addEventListener('submit', addComment)
 
+
+// ********* fetch inital image *************
 function fetchMyImage(){
   fetch(`${imageURL}`)
   .then(resp => resp.json())
@@ -27,45 +26,65 @@ function fetchMyImage(){
     imageName.textContent = json.name
     imageLikes.textContent = json.like_count
     json.comments.forEach((comment) => {
-      const li = document.createElement('li')
-      li.textContent = comment.content
-      imageCommentsUl.appendChild(li)
+      updateWithNewComment(comment.content, comment.id)
     })
   })
-} // end of fetch image
+}
+// ******************************************
 
-//
+// ****** add a new like *************************
 function addLike(json){
-// console.log(json)
-imageLikes.textContent = json.like_count +=1
+  imageLikes.textContent = json.like_count +=1
+  fetch('https://randopic.herokuapp.com/likes', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+          image_id: 2282
+      })
+  })
+  .then(response => response.json())
 
-fetch('https://randopic.herokuapp.com/likes', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify({
-                image_id: 2282
-            })
-        })
-.then(response => response.json())
+}
+//****************************************************
 
-}// end of add like function
-
-commentForm.addEventListener('submit', addComment)
-
+// ****** Add a new comment ***************************
 function addComment(e){
   e.preventDefault()
   const commentText = document.getElementById('comment_input')
   const newComment = commentText.value
   const imageCommentsUl = document.getElementById('comments')
-
-  const li = document.createElement('li')
-  li.textContent = newComment
-  imageCommentsUl.appendChild(li)
+  // updateWithNewComment(newComment)
   commentForm.reset();
   updateCommentDb(newComment)
+
+}
+
+function updateWithNewComment(commentText, commentId){
+  const li = document.createElement('li')
+  li.textContent = commentText
+  li.classList.add('list-group-item')
+  li.id = commentId
+  const deleteBtn = document.createElement('button')
+  deleteBtn.textContent = 'delete'
+  deleteBtn.classList.add("btn-danger")
+  deleteBtn.addEventListener('click', () => {
+    deleteItem(li)
+  })
+  li.appendChild(deleteBtn)
+  imageCommentsUl.appendChild(li)
+}
+// ****************************************************
+
+
+// ******** Delete a comment ************************
+function deleteItem(li){
+  imageCommentsUl.removeChild(li)
+  fetch(`https://randopic.herokuapp.com/comments/${li.id}`, {
+    method: "DELETE"
+  })
 }
 
 function updateCommentDb(newComment){
@@ -79,27 +98,15 @@ function updateCommentDb(newComment){
       image_id: 2282,
       content: newComment
     })
-  }).then(resp => resp.json())
+  })
+  .then(resp => resp.json())
+  .then( data => {
+    updateWithNewComment(data.content, data.id)
+  })
 }
 
+// *********************************************************
 
-
-
+// ** Actually call the fetch to start the whole thing rolling **
 fetchMyImage();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
